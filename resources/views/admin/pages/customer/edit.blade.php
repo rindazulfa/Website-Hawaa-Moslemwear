53595ad16aadb919926743b02e266518a17201cc
dimazivan <dimazivan740@gmail.com>
2021-06-15 20:20:23 +0700

    Purchase kecil + keci" + dimaz unyuk

--- resources/views/admin/pages/customer/edit.blade.php
+++ resources/views/admin/pages/customer/edit.blade.php
@@ -8,9 +8,9 @@
                     <h6 class="h2 text-white d-inline-block mb-0">Tables</h6>
                     <nav aria-label="breadcrumb" class="d-none d-md-inline-block ml-md-4">
                         <ol class="breadcrumb breadcrumb-links breadcrumb-dark">
-                            <li class="breadcrumb-item"><a href="#"><i class="fas fa-home"></i></a></li>
-                            <li class="breadcrumb-item"><a href="#">Data Customer</a></li>
-                            <li class="breadcrumb-item active" aria-current="page">Form Data Customer</li>
+                            <li class="breadcrumb-item"><a href="/admin"><i class="fas fa-home"></i></a></li>
+                            <li class="breadcrumb-item"><a href="/customer">Data Customer</a></li>
+                            <li class="breadcrumb-item active" aria-current="page">Form Edit Data Customer</li>
                         </ol>
                     </nav>
                 </div>
