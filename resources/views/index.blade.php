9159102c20993abc7557a4795e387ba1d9fc4494
dimazivan <dimazivan740@gmail.com>
2021-08-02 09:59:29 +0700

    fix v6

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,15 +2,15 @@
 @section('content')
 <!-- popup  -->
 <div class="popup-wrapper" id="popup">
-        <div class="popup-container">    
-            <!-- Konten popup -->
-            <a href="#">
-                <img width="100%" height="100%" src="{{'uploads/banner/payment.png'}}" alt="payment.png">
-            </a>
-            <!-- <img src="Gambar/" alt=""> -->
-            <a class="popup-close" href="#popup">X</a>
-        </div>
+    <div class="popup-container">
+        <!-- Konten popup -->
+        <a href="{{asset('uploads/banner/payment.png')}}" target="_blank">
+            <img width="100%" height="100%" src="{{'uploads/banner/payment.png'}}" alt="payment.png">
+        </a>
+        <!-- <img src="Gambar/" alt=""> -->
+        <a class="popup-close" href="#popup">X</a>
     </div>
+</div>
 @if (isset($banner->picture))
 <div class="hero-wrap js-fullheight" style="background-image: url({{'uploads/banner/'.$banner->picture}});">
     <div class="overlay"></div>
@@ -32,57 +32,55 @@
 </div>
 @endif
 
-
-<div class="goto-here"></div>
-
-<section id="promo" class="ftco-section ftco-product">
-    <div class="container">
-        <div class="row justify-content-center mb-3 pb-3">
-            <div class="col-md-12 heading-section text-center ftco-animate">
-                <h1 class="big">Product</h1>
-                <h2 class="mb-4">Product</h2>
+<div class="goto-here">
+    <section id="promo" class="ftco-section ftco-product">
+        <div class="container">
+            <div class="row justify-content-center mb-3 pb-3">
+                <div class="col-md-12 heading-section text-center ftco-animate">
+                    <h1 class="big">Product</h1>
+                    <h2 class="mb-4">Product</h2>
+                </div>
             </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <div class="product-slider owl-carousel ftco-animate">
-                    @forelse($shop as $key)
-                    <div class="item">
-                        <div class="product">
-                            <a href="{{route('shop.show',[$key->id])}}" class="img-prod"><img class="img-fluid" src="{{'uploads/products/'.$key->pict_1}}" alt="Colorlib Template">
-                            </a>
-                            <div class="text py-3 px-3">
-                                <h3><a href="#">{{$key->name}}</a></h3>
-                                <div class="d-flex">
-                                    <div class="pricing">
-                                        <p class="price"><span class="price-sale">Rp {{number_format($key->price,2,',','.')}}</span></p>
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="product-slider owl-carousel ftco-animate">
+                        @forelse($shop as $key)
+                        <div class="item">
+                            <div class="product">
+                                <a href="{{route('shop.show',[$key->id])}}" class="img-prod"><img class="img-fluid" src="{{'uploads/products/'.$key->pict_1}}" alt="Colorlib Template">
+                                </a>
+                                <div class="text py-3 px-3">
+                                    <h3><a href="#">{{$key->name}}</a></h3>
+                                    <div class="d-flex">
+                                        <div class="pricing">
+                                            <p class="price"><span class="price-sale">Rp {{number_format($key->price,2,',','.')}}</span></p>
+                                        </div>
                                     </div>
+                                    @guest
+                                    @else
+                                    <hr>
+                                    <form action="{{ route('store') }}" method="post">
+                                        @csrf
+                                        <input type="text" name="id" id="id" value="{{ $key->id }}" hidden readonly>
+                                        <input type="number" name="price" id="price" value="{{ $key->price }}" hidden readonly>
+                                        <input type="text" name="size" id="size" value="1" hidden readonly>
+                                        <input type="number" name="qty" id="qty" value="1" hidden readonly>
+                                        <input type="date" name="date" id="date" value="<?php echo date('Y-m-d'); ?>" hidden readonly>
+                                        <p class="bottom-area d-flex">
+                                            <button class="add-to-cart" type="submit"><span>Tambah Keranjang<i class="ion-ios-add ml-1"></i></span></button>
+                                        </p>
+                                    </form>
+                                    @endguest
                                 </div>
-                                @guest
-                                @else
-                                <hr>
-                                <form action="{{ route('store') }}" method="post">
-                                    @csrf
-                                    <input type="text" name="id" id="id" value="{{ $key->id }}" hidden readonly>
-                                    <input type="number" name="price" id="price" value="{{ $key->price }}" hidden readonly>
-                                    <input type="text" name="size" id="size" value="1" hidden readonly>
-                                    <input type="number" name="qty" id="qty" value="1" hidden readonly>
-                                    <input type="date" name="date" id="date" value="<?php echo date('Y-m-d'); ?>" hidden readonly>
-                                    <p class="bottom-area d-flex">
-                                        <button class="add-to-cart" type="submit"><span>Tambah Keranjang<i class="ion-ios-add ml-1"></i></span></button>
-                                    </p>
-                                </form>
-                                @endguest
                             </div>
                         </div>
-                    </div>
-                    <!-- </form> -->
-                    @empty
-                    <tr>
-                        <td colspan="7" class="text-center">Data Kosong</td>
-                    </tr>
-                    @endforelse
-                    <!-- <div class="item">
+                        <!-- </form> -->
+                        @empty
+                        <tr>
+                            <td colspan="7" class="text-center">Data Kosong</td>
+                        </tr>
+                        @endforelse
+                        <!-- <div class="item">
                         <div class="product">
                             <a href="/detailproduct" class="img-prod"><img class="img-fluid" src="images/product-7.png" alt="Colorlib Template">
                                 <span class="status">30%</span>
@@ -97,35 +95,36 @@
                             </div>
                         </div>
                     </div> -->
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-</section>
+    </section>
+</div>
 @if (isset($footer))
 <section class="ftco-section ftco-no-pb ftco-no-pt">
-	<div class="container">
+    <div class="container">
         <div class="row justify-content-center mb-10 pb-4">
             <div class="col-md-12 heading-section text-center ftco-animate">
                 <h1 class="big">About Us</h1>
             </div>
         </div>
-		<div class="row mt-md-5">
-			<div class="col-md-5 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url({{'uploads/profil/'.$footer->picture}});">
-			</div>
-			<div class="col-md-7 py-5 wrap-about pb-md-5 ftco-animate">
-				<div class="heading-section-bold mb-5 mt-md-5">
-					<div class="ml-md-0">
-						<h2 class="mb-4">Hawaa Moslemwear</span></h2>
-					</div>
-				</div>
-				<div class="pb-md-5">
-					<p>{{$footer->desc_1}}</p>
-					<p>{{$footer->desc_2}}</p>
-				</div>
-			</div>
-		</div>
-	</div>
+        <div class="row mt-md-5">
+            <div class="col-md-5 p-md-5 img img-2 d-flex justify-content-center align-items-center" style="background-image: url({{'uploads/profil/'.$footer->picture}});">
+            </div>
+            <div class="col-md-7 py-5 wrap-about pb-md-5 ftco-animate">
+                <div class="heading-section-bold mb-5 mt-md-5">
+                    <div class="ml-md-0">
+                        <h2 class="mb-4">Hawaa Moslemwear</span></h2>
+                    </div>
+                </div>
+                <div class="pb-md-5">
+                    <p>{{$footer->desc_1}}</p>
+                    <p>{{$footer->desc_2}}</p>
+                </div>
+            </div>
+        </div>
+    </div>
 </section>
 @endif
 
@@ -167,7 +166,7 @@
                     </div>
                     <div class="media-body">
                         <h3 class="heading">Garansi Pengembalian</h3>
-                        <p>Kami memberikan garansi pengembalian terhadap produk yang anda pesan melalui halaman website kami, 
+                        <p>Kami memberikan garansi pengembalian terhadap produk yang anda pesan melalui halaman website kami,
                             apabila terjadi ketidakkesesuian dengan produk yang anda pesan. Syarat & Ketentuan Belaku</p>
                     </div>
                 </div>
